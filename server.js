0a5e4ecea69fc7a3c8ac1a6c8491c8c69714c48d
Keri Russin <karussin@student.fullsail.edu>
2022-04-09 22:29:11 -0400

    Initial Project Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.json({
+        message: 'Using - GET',
+    });
+});
+
+
+
+// middleware modules for error handling
+// * * use this in all of your server.js files
+app.use((req, res, next) => {
+    const error = new Error('NOT FOUND!!!');
+    error.status = 404;
+    next(error);
+    });
+    // middleware to send error nicely
+    app.use((error, req, res, next) => {
+        res.status(error.status || 500).json({
+            error: {
+                message: error.message, 
+                status: error.status,
+                method: req.method
+            }
+        });
+    })
+
+
+app.listen(process.env.port, () => console.log(`Starting server on port ${process.env.port}`))
